ee86c146b6d284003d56f18e9b370f887a7a4902
Stanislav Valasek <valasek@gmail.com>
2023-03-04 19:45:44 +0100

    netlify deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,14 +13,24 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		// adapter: adapter()
-		adapter: adapter({
+		//adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
+		//	pages: 'build',
+		//	assets: 'build',
+		//	fallback: null,
+		//	precompress: false,
+		//	strict: true
+		//})
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
 		})
 	}
 };
